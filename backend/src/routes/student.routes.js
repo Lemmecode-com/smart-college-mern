73a8ac50baedfacc6ae49f67d4f101f25a78671c
Rohidas Lakade <rohidaslakade0707@gmail.com>
2026-01-12 12:38:35 +0530

    Added files like student and user model also same for frontend

---
+++ backend/src/routes/student.routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const auth = require("../middleware/auth.middleware");
+const role = require("../middleware/role.middleware");
+const ctrl = require("../controllers/student.controller");
+
+// Admin only: create student
+router.post("/", auth, role("admin"), ctrl.createStudent);
+
+// Admin & Teacher: view students (secure)
+router.get("/", auth, role("admin", "teacher"), ctrl.getStudents);
+
+// Get student by ID
+router.get("/:id", auth, ctrl.getStudentById);
+
+module.exports = router;
