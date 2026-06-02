9e29e3a55e25f65a16bfea9a7940f90d54fdaee0
Rutika Khabale <rutikakhabale8@gmail.com>
2026-06-02 10:59:57 +0530

    fix(backend): add college isolation to ParentGuardian model
    - Add required college_id field to ParentGuardian schema
    - Add query-level pre-hooks on find/findOne/findOneAndUpdate
    - Populate college_id from student in parentCreation service
    - Require college_id in parent controller/middleware
    
    Prevents parents from accessing cross-college student data.

--- backend/src/models/parentGuardian.model.js
+++ backend/src/models/parentGuardian.model.js
@@ -6,6 +6,11 @@ const parentGuardianSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+  college_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "College",
+    index: true,
+  },
   student_ids: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -21,6 +26,27 @@ const parentGuardianSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+parentGuardianSchema.index({ college_id: 1 }, { sparse: true });
+
+// 🔒 COLLEGE ISOLATION: Prevent cross-college ParentGuardian lookups/updates
+parentGuardianSchema.pre("find", function () {
+  if (this.getQuery().college_id) {
+    this.where({ college_id: this.getQuery().college_id });
+  }
+});
+
+parentGuardianSchema.pre("findOne", function () {
+  if (this.getQuery().college_id) {
+    this.where({ college_id: this.getQuery().college_id });
+  }
+});
+
+parentGuardianSchema.pre("findOneAndUpdate", function () {
+  if (this.getQuery().college_id) {
+    this.where({ college_id: this.getQuery().college_id });
+  }
 });
 
 module.exports = mongoose.model("ParentGuardian", parentGuardianSchema);
