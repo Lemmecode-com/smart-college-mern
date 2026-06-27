fdc8b223b44aa0aa8c45df5c4b2a27aff7b12357
deploy <deploy@edunovaa-prod>
2026-06-27 15:05:52 +0530

    feat: integrate GlitchTip via @sentry/react v7 on frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+Sentry.init({
+  dsn: "https://307cf5e6f3bb4876a249d0d81073e8ae@errors.restromonitor.com/7",
+  tracesSampleRate: 0,
+  autoSessionTracking: false,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
